320dcb2973025adf929b92efcf0a0f6fad5b082b
CasellaJr <casella0798@gmail.com>
2025-04-10 09:42:23 +0000

    validation and exchange of amtrices

--- code/fed2rc.py
+++ code/fed2rc.py
@@ -11,6 +11,7 @@ from sktime.transformations.panel.rocket import Rocket
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.linear_model import RidgeClassifierCV
+from sklearn.model_selection import train_test_split
 from utils import load_dataset, preprocess_data, split_function, RocketKernel, transform_seeds, get_binary_dataset_names, get_three_classes_dataset_names, get_four_classes_dataset_names, get_multiclasses_dataset_names
 from sklearn.preprocessing import LabelBinarizer
 
@@ -18,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--clients', type=int, default=4, help='Choose the number of parties of the federation')
 parser.add_argument('-k', '--kernels', type=int, default=100, help='Choose the number of ROCKET kernels to use')
 parser.add_argument('-r', '--rounds', type=int, default=100, help='Choose the number of rounds of federated training')
-parser.add_argument('-l', '--regularizer', action='store_true', help='Choose this option to enable FedAvg on the Ridge regularizer (mean of the local lambdas). Otherwise, default value set to 0.01')
+parser.add_argument('-v', '--validation', action='store_true', help='Choose this option to enable a grid search for the lambda regularizer on a validation set.')
 parser.add_argument('--debug', action='store_true', help='Use this option to disable WandB metrics tracking')
 args = parser.parse_args()
 
@@ -34,7 +35,8 @@ def main():
     #list_of_datasets = get_three_classes_dataset_names()
     #list_of_datasets = get_four_classes_dataset_names()
     #list_of_datasets = get_multiclasses_dataset_names()
-    list_of_datasets = ['Adiac'] #example
+    #list_of_datasets = ['PigCVP'] #example
+    list_of_datasets = get_binary_dataset_names() + get_three_classes_dataset_names() + get_four_classes_dataset_names() + get_multiclasses_dataset_names()
 
     if args.debug:
         wandb.init(mode="disabled")
@@ -62,8 +64,11 @@ def main():
         else:
             entity = "mlgroup"
             project = "Fed2RC"
-            run_name = f"{ds_name}_{n_kernels}KERNELS_{n_clients}CLIENTS"
-            tags = ["Binary"]
+            if args.validation:
+                run_name = f"VAL_{ds_name}_{n_kernels}KERNELS_{n_clients}CLIENTS"
+            else:
+                run_name = f"{ds_name}_{n_kernels}KERNELS_{n_clients}CLIENTS"
+            tags = ["Fed2RC"]
             if run_name in run_names:
                 print(f"Experiment {run_name} already executed.")
                 continue
@@ -76,9 +81,20 @@ def main():
             np.random.seed(experiment_seed)
             random.seed(experiment_seed)
             os.environ['PYTHONHASHSEED'] = str(experiment_seed)
-
-            s_X_train, s_Y_train = split_function(X_train, Y_train, n_clients, rng)
-            s_X_test, s_Y_test = split_function(X_test, Y_test, n_clients, rng)
+            
+            if ds_name in ["DiatomSizeReduction", "Fungi", "PigAirwayPressure", "PigArtPressure", "PigCVP"]:
+                split = "random"
+                s_X_Y_train = split_function(X_train, Y_train, n_clients, rng, my_split=split)
+                s_X_Y_test = split_function(X_test, Y_test, n_clients, rng, my_split=split)
+                s_X_train = [x for x, _ in s_X_Y_train]
+                s_Y_train = [y for _, y in s_X_Y_train]
+                s_X_test = [x for x, _ in s_X_Y_test]
+                s_Y_test = [y for _, y in s_X_Y_test]
+            else:
+                split='uniform'
+                s_X_train, s_Y_train = split_function(X_train, Y_train, n_clients, rng, my_split=split)
+                s_X_test, s_Y_test = split_function(X_test, Y_test, n_clients, rng, my_split=split)
+            
 
             # Initialize different seeds for all clients
             seeds = np.arange(n_clients * n_kernels).reshape(n_clients, n_kernels)
@@ -99,15 +115,15 @@ def main():
             selected_seeds = {}
 
             for epoch in range(n_rounds +1):
-    
                 train_accs = np.array([], dtype=float)
                 round_selected_seeds = np.array([], dtype=int)
                 train_accuracies = np.array([], dtype=float)
                 lambdas = np.array([], dtype=float)
                 for client_id in range(n_clients):
                     x_train, y_train = s_X_train[client_id], s_Y_train[client_id]
+                    if args.validation:
+                        x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.1, random_state=experiment_seed)
                     client_seeds = dict_seeds[client_id]
-                    
                     x_trans = transform_seeds(x_train, client_seeds, ts_length)
                     model = RidgeClassifierCV(alphas=np.logspace(-3,0,100)).fit(x_trans, y_train)
                     w = model.coef_[0] 
@@ -141,6 +157,7 @@ def main():
                     
             A_global = np.zeros((final_num_kernels, final_num_kernels))
             b_global = np.zeros((final_num_kernels, n_classes))
+            A_locals, b_locals = [], []
             for client_id in range(n_clients):
                 x_train, y_train = s_X_train[client_id], s_Y_train[client_id]
                 client_seeds = dict_seeds[client_id]
@@ -148,17 +165,52 @@ def main():
                 y_onehot = np.eye(n_classes)[y_train]
                 A_local = x_trans.T @ x_trans
                 b_local = x_trans.T @ y_onehot
+                A_locals.append(A_local)
+                b_locals.append(b_local)
                         
                 A_global += A_local
                 b_global += b_local
 
-            if args.regularizer:        
-                print(f"Lambdas: {lambdas}", f"Mean lambda: {np.sum(lambdas)/np.sum(train_accs)}") 
-                lambda_reg = np.sum(lambdas)/np.sum(train_accs)
-            else:
-                lambda_reg = 0.01
-                    
-            A_final = A_global + lambda_reg * np.eye(A_global.shape[0])
+
+            #cross validation for best lambda
+            local_best_lambdas = np.array([])
+            local_best_accs = np.array([])
+            tmp_lambda = 0
+            for client_id in range(n_clients):
+                best_accuracy = 0
+                x_train, y_train = s_X_train[client_id], s_Y_train[client_id]
+                if args.validation:
+                    _, x_val, _, y_val = train_test_split(x_train, y_train, test_size = 0.1, random_state=experiment_seed)
+                client_seeds = dict_seeds[client_id]
+                x_trans = transform_seeds(x_train, client_seeds, ts_length)  
+                if args.validation:
+                    x_trans = transform_seeds(x_val, client_seeds, ts_length)
+                y_onehot = np.eye(n_classes)[y_train]
+
+                A_minus_client = A_global - A_locals[client_id]
+                b_minus_client = b_global - b_locals[client_id]
+                
+                for cv_lambda in np.logspace(-3,0,100):
+                    if args.validation == False:
+                        local_A_final = A_minus_client + cv_lambda * np.eye(A_global.shape[0])
+                        local_W_final = np.linalg.solve(local_A_final, b_minus_client)
+                    else:
+                        local_A_final = A_global + cv_lambda * np.eye(A_global.shape[0])
+                        local_W_final = np.linalg.solve(local_A_final, b_global)
+                    local_W_final =  local_W_final / (np.linalg.norm(local_W_final, axis=0, keepdims=True) + 1e-10)
+                    local_y_pred = np.argmax(x_trans @ local_W_final, axis=1)
+                    if args.validation == False:
+                        local_accuracy = accuracy_score(y_train, local_y_pred)
+                    else:
+                        local_accuracy = accuracy_score(y_val, local_y_pred)
+                    if best_accuracy < local_accuracy or (best_accuracy == local_accuracy and cv_lambda > tmp_lambda):
+                        tmp_lambda = cv_lambda
+                        best_accuracy = local_accuracy
+                    #print("Client: ", client_id, "best local accuracy: ", local_accuracy, "with lambda: ", cv_lambda)
+                local_best_lambdas = np.concatenate([local_best_lambdas, [tmp_lambda * best_accuracy]])
+                local_best_accs = np.concatenate([local_best_accs, [best_accuracy]])
+
+            A_final = A_global + np.sum(local_best_lambdas)/np.sum(local_best_accs) * np.eye(A_global.shape[0])
             W_final = np.linalg.solve(A_final, b_global)
             W_final =  W_final / (np.linalg.norm(W_final, axis=0, keepdims=True) + 1e-10)
             X_test_trans = transform_seeds(X_test, round_selected_seeds, ts_length)
@@ -168,14 +220,21 @@ def main():
                 test_f1 = f1_score(Y_test, y_pred)
             else:
                 test_f1 = f1_score(Y_test, y_pred, average = 'macro')
-                    
-            print(f"\nFinal Test Accuracy: {test_accuracy:.4f}, Number of unique seeds: {final_num_kernels}")
-            wandb.log({f'Test accuracy {experiment_seed}': test_accuracy,
-                        f'Test F1 {experiment_seed}': test_f1,
-                        f'Lambda Regularizer': lambda_reg, 
-                        f'Rounds': epoch,
-                        f'Kernels': final_num_kernels})
-            
+
+            print(f"Test Accuracy: {test_accuracy:.4f}, Test F1: {test_f1}, Number of unique seeds: {n_kernels}")
+
+            if args.validation:    
+                wandb.log({f'Test Accuracy using a regularizer coming from a validation set ({experiment_seed})': test_accuracy,
+                            f'Test F1 using a regularizer coming from a validation set ({experiment_seed})': test_f1,
+                            f'Validation Regularizer': np.sum(local_best_lambdas)/np.sum(local_best_accs),
+                            f'Rounds': ({epoch}),
+                            f'Kernels': final_num_kernels})
+            else:
+                wandb.log({f'Test Accuracy ({experiment_seed})': test_accuracy,
+                            f'Test F1 ({experiment_seed})': test_f1,
+                            f'Validation Regularizer': np.sum(local_best_lambdas)/np.sum(local_best_accs),
+                            f'Rounds': ({epoch}),
+                            f'Kernels': final_num_kernels})
         wandb.finish()
 
 if __name__ == '__main__':
